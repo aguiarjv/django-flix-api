36012fcf2095bb3930f4c2beee9fab6cfc31516c
aguiarjv <jvictoraguiar@live.com>
2024-01-24 17:04:44 -0300

    genre list view using drf

--- genres/views.py
+++ genres/views.py
@@ -3,6 +3,13 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from genres.models import Genre
+from rest_framework import generics
+from genres.serializers import GenreSerializar
+
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializar
 
 
 @csrf_exempt
