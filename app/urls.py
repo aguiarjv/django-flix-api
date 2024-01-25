8f41b5abd0eaacb0bbc2b0e64e7fb00593247e15
aguiarjv <jvictoraguiar@live.com>
2024-01-25 20:00:36 -0300

    created CRUD for movies app

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,4 +14,8 @@ urlpatterns = [
     path("actors/", ActorCreateListView.as_view(), name="actor-create-list"),
     path("actors/<int:pk>/", ActorRetrieveUpdateDestroyView.as_view(),
          name="actor-detail-list"),
+
+    path("movies/", MovieCreateListView.as_view(), name="movie-create-list"),
+    path("movies/<int:pk>/", MovieRetrieveUpdateDestroyView.as_view(),
+         name="movie-detail-list"),
 ]
