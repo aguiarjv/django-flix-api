d77ed403463361b1dbd99d457097d61f9db949eb
aguiarjv <jvictoraguiar@live.com>
2024-01-25 19:24:26 -0300

    created CRUD operations for actors

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("genres/", GenreCreateListView.as_view(), name="genre-create-list"),
     path("genres/<int:pk>/", GenreRetrieveUpdateDestroyView.as_view(),
          name="genre-detail-list"),
+
+    path("actors/", ActorCreateListView.as_view(), name="actor-create-list"),
+    path("actors/<int:pk>/", ActorRetrieveUpdateDestroyView.as_view(),
+         name="actor-detail-list"),
 ]
