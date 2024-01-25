69337d96be422b00ae41be138ee4e2a3a31d7f71
aguiarjv <jvictoraguiar@live.com>
2024-01-25 17:46:22 -0300

    created actors app

---
+++ actors/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+NATIONALITY_CHOICE = (
+    ('USA', 'Estados Unidos'),
+    ('BRAZIL', 'Brasil'),
+
+)
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(null=True, blank=True)
+    nationality = models.CharField(
+        max_length=100, choices=NATIONALITY_CHOICE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
