398771649b4e7d1b1e3a32e89b6e85d0e0324f02
aguiarjv <jvictoraguiar@live.com>
2024-11-24 11:17:50 -0300

    added rate field

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,19 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        stars_avg = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if stars_avg:
+            return round(stars_avg, 1)
+
+        return None
